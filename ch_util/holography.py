1c2fc0fd752ab981bec538e0c4024691d6c9fab6
Alex Hill coconut <ashill@astro.ubc.ca>
2019-11-05 15:07:28 -0800

    fix(holography) : fix time zome bug in creating db entries Was relying on .strftime("%s") to get time in Unix seconds. This was producing results in the wrong time zone, improperly converting to UTC. Use ephemeris.ensure_unix() instead, which is better than going through a string anyway.
    Also change print statement to use %z (+0000) instead of %Z (UTC).

--- ch_util/holography.py
+++ ch_util/holography.py
@@ -269,12 +269,9 @@ class HolographyObservation(base_model):
                 print("Missing", srcnm)
                 output_params["src"] = srcnm
 
-            output_params["finish_time"] = ts.utc(
-                ephemeris.unix_to_datetime(
-                    float(output_params["start_time"].utc_strftime("%s"))
-                    + output_params["DURATION"] * 3600.0 * ephemeris.SIDEREAL_S
-                )
-            )
+            output_params["finish_time"] = ephemeris.unix_to_skyfield_time(
+                    ephemeris.ensure_unix(output_params["start_time"])
+                    + output_params["DURATION"] * 3600.0 * ephemeris.SIDEREAL_S )
 
             output_params["quality_flag"] = QUALITY_GOOD
 
@@ -305,7 +302,7 @@ class HolographyObservation(base_model):
         from skyfield.positionlib import Angle
 
         ts = ephemeris.skyfield_wrapper.timescale
-        DATE_FMT_STR = "%Y-%m-%d %H:%M:%S %Z"
+        DATE_FMT_STR = "%Y-%m-%d %H:%M:%S %z"
 
         pr_list, al_list = cls.parse_ant_logs(logs, return_post_report_params=True)
 
@@ -442,9 +439,7 @@ class HolographyObservation(base_model):
             """
             ts = ephemeris.skyfield_wrapper.timescale
 
-            # precise only to one second; should be good enough for searching
-            # database
-            unixt = float(t.utc_strftime("%s"))
+            unixt = ephemeris.ensure_unix(t)
 
             dup_found = False
 
@@ -546,8 +541,8 @@ class HolographyObservation(base_model):
             else:
                 cls.create(
                     source=dict["src"],
-                    start_time=float(dict["start_time"].utc_strftime("%s")),
-                    finish_time=float(dict["finish_time"].utc_strftime("%s")),
+                    start_time=ephemeris.ensure_unix(dict["start_time"]),
+                    finish_time=ephemeris.ensure_unix(dict["finish_time"]),
                     quality_flag=dict["quality_flag"],
                     notes=notes,
                 )
