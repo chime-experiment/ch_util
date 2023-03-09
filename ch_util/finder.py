4ac13a8ed36b57134408eb4c699e2db09aaa0a4f
ljgray <liam.j.gray@usask.ca>
2023-03-09 11:11:12 -0800

    feat(finder): add ntries argument to Finder

--- ch_util/finder.py
+++ ch_util/finder.py
@@ -239,7 +239,7 @@ class Finder(object):
     # Constructors and setup
     # ----------------------
 
-    def __init__(self, acqs=(), node_spoof=None):
+    def __init__(self, acqs=(), node_spoof=None, ntries=1):
         import copy
 
         # Which nodes do we have available?
@@ -247,7 +247,7 @@ class Finder(object):
         self._my_node = []
         self._node_spoof = node_spoof
 
-        connect_database()
+        connect_database(ntries=ntries)
 
         if not node_spoof:
             for n in (
