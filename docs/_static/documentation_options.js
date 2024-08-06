38b8769a6b1647b4e313e6cdbf4f0a5b417b964b
Rick Nitsche (automated) <nritsche@users.noreply.github.com>
2024-08-06 17:14:01 +0000

    Automated sphinx build

---
+++ docs/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '24.8.0.post1+git.f9dfc4b8',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
